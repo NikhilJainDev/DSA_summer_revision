78b0648f2732a5caf352df590eeb0a72e69a1106
Nikhil jain <nikhiljain90598@gmail.com>
2025-06-11 19:12:38 +0530

    STL in C++

--- STL.cpp
+++ STL.cpp
@@ -1,6 +1,10 @@
 #include<iostream>
 #include<algorithm>
+#include<list>
 #include<vector>
+#include<deque>
+#include<stack>
+#include<queue>
 using namespace std;
 
 int main(){
@@ -30,7 +34,60 @@ int main(){
     arr.clear(); // clear the vector All elements 
 
     cout<<" Check that vector is empty or not "<<arr.empty()<<endl;
-    
+
+    // List in c++ - It is represented as Doubly link list 
+
+    list<int> li = {1,2,3};
+
+    li.push_back(4);
+    li.pop_back();
+    li.push_front(11);
+    li.push_front(5);
+    li.pop_front();
+
+    for(int val : li){
+        cout<<val<<" ";
+    }
+    cout<<endl;
+
+    // dequeue in C++  - Double ended queue 
+
+    deque<int> d = {11,21,51};
+    cout<<"Elements of deque are : "<<" ";
+    for(int elm : d){
+        cout<<elm<<" ";
+    }
+
+    cout<<endl;
+
+    // Pairs in c++ 
+    pair<string, int> pr = {"Nikhil", 20};
+    cout<<"First Value in Pair is : "<<pr.first<<endl;
+    cout<<" Second Value in Pair is : "<<pr.second<<endl;
+
+    // we can also create the pair of pairs 
+    pair<string, pair<string, int>> info = {"Janu Jain", {"Btech From Poornima College", 20}};
+
+    cout<<" Pair of Pairs Info is Fetched as -> "<<endl;
+    cout<<info.first<<endl;
+    cout<<info.second.first<<endl;
+    cout<<info.second.second<<endl;
+
+    // Sorting in vector
+    vector<int> vc  = {1,9,8,4,2,3,5,6};
+
+    cout<<" Sorting in Ascending order is  : "<<" ";
+    sort(vc.begin(), vc.end());
+    for(int el : vc){
+        cout<<el<<" ";
+    }
+    cout<<endl;
+    cout<<" Sorting in Descending order is : "<<" ";
+    sort(vc.begin(), vc.end(), greater<int>());
+    for(int el : vc){
+        cout<<el<<" ";
+    }
+
 
     return 0;
 }
