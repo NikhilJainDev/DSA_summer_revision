af0d4c4d5d6b57c482f8d2b5d404591f2d2b91a3
Nikhil jain <nikhiljain90598@gmail.com>
2025-06-11 18:08:01 +0530

    STL in C++

--- STL.cpp
+++ STL.cpp
@@ -1,7 +1,36 @@
 #include<iostream>
+#include<algorithm>
+#include<vector>
 using namespace std;
 
 int main(){
+    vector<int> vec;
+    vec.push_back(1);
+    vec.push_back(2);
+    vec.push_back(3);
+
+    cout<<" Now Size of Vector is : "<<vec.size()<<endl;
+    cout<<" Capacity of Vector is : "<<vec.capacity()<<endl;
+
+    vec.pop_back();
+
+    cout<<" After deleting the element size of vector becomes -> "<<vec.size()<<endl;
+
+    vector<int> arr = {87,40,59,61,12,55,63};
+    sort(arr.begin(), arr.end());
+
+    for(int el : arr){
+        cout<<el<<" ";
+    }
+    cout<<endl;
+
+    cout<<" Fornt element on vector is : "<<arr.front();
+    cout<<" Back element on vector is : "<<arr.back()<<endl;
+
+    arr.clear(); // clear the vector All elements 
+
+    cout<<" Check that vector is empty or not "<<arr.empty()<<endl;
+    
 
     return 0;
 }
