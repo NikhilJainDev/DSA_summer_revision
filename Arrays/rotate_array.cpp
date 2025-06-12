054d57eecfe4ebeacb9d7654250c039d54b84305
Nikhil jain <nikhiljain90598@gmail.com>
2025-06-12 14:44:27 +0530

    Array Problems

--- Arrays/rotate_array.cpp
+++ Arrays/rotate_array.cpp
@@ -1,8 +1,25 @@
 #include<iostream>
 #include<vector>
 using namespace std;
+
+int RotateArray(vector<int> array){
+    int last = array[array.size()-1];
+     for(int i= array.size()-1; i>=1; i--){
+        array[i] = array[i-1];
+    }
+    array[0] = last;
+
+    for(int el : array){
+        cout<<el<<" ";
+    }
+    cout<<endl;
+}
+
+
 int main(){
-    
+    vector<int> array = {1,2,3,4,5};
+
+    RotateArray(array);
     
 return 0;
 }
