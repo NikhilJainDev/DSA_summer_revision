6d91f73d3f8669f2e240522bf9469c32e39aedf3
Nikhil jain <nikhiljain90598@gmail.com>
2025-06-12 14:09:28 +0530

    Array Problems

--- Arrays/largest_smallest.cpp
+++ Arrays/largest_smallest.cpp
@@ -14,7 +14,16 @@ using namespace std;
     }
 
     int secondLargestElement(vector<int> vec){
-
+        int largest_elemnt = largestElement(vec);
+        int soln = INT32_MIN;
+        for(int i=0; i<vec.size(); i++){
+            if(vec[i] != largest_elemnt){
+                if(vec[i] > soln){
+                    soln = vec[i];
+                }
+            }
+        }
+        return soln;
     }
 
     int SmallestElement(vector<int> vec){
@@ -28,17 +37,26 @@ using namespace std;
     }
 
     int SecondSmallestElement(vector<int> vec){
-
+        int soln = INT32_MAX;
+        int smallest_elemnt = SmallestElement(vec);
+        for(int i=0; i<vec.size(); i++){
+            if(vec[i] != smallest_elemnt){
+                if(vec[i] < soln){
+                    soln = vec[i];
+                }
+            }
+        }
+        return soln;
     }
 
 
 int main(){
 
-    vector<int> vec = {42, 17, 89, 3, 56, 73, 28, 64, 91, 101, 7, 38, 67, 25, 99};
+    vector<int> vec = {42, 17, 89, 3, 100, 73, 28, 64, 91, 101, 7, 38, 67, 25, 99};
     cout<<" Larget Element is -> "<<largestElement(vec)<<endl;
-    cout<<secondLargestElement(vec)<<endl;
+    cout<<" Second Larget element is : -> "<<secondLargestElement(vec)<<endl;
     cout<<" Smallest Element is -> "<<SmallestElement(vec)<<endl;
-    cout<<SecondSmallestElement(vec)<<endl;
+    cout<<" Second Smallest Element is -> "<<SecondSmallestElement(vec)<<endl;
     
 return 0;
 }
