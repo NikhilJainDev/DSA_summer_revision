6f48945f36cb2b767e546a48cd6c8be035e0b922
Nikhil jain <nikhiljain90598@gmail.com>
2025-06-12 19:07:20 +0530

    Recursion Problems

--- Recursion/lecture1.cpp
+++ Recursion/lecture1.cpp
@@ -1,6 +1,27 @@
 #include<iostream>
 using namespace std;
-int main()
-{
+
+int factorial(int number){
+    if(number == 0){
+        return 1;
+    }
+    else{
+        return number * factorial(number-1);
+    }
+}
+// Sum of N numbers 
+int sum_N_Numbers(int n){
+    if(n == 1){
+        return n;
+    }
+    else{
+        return n + sum_N_Numbers(n-1);
+    }
+}
+int main(){
+    cout<< factorial(5);
+    cout<<endl;
+    cout<< sum_N_Numbers(10); cout<<endl;
+    cout<<sum_N_Numbers(20);
 return 0;
 }
