4e6663c41c0801b47146f5323c7061749b56ef7b
Nikhil jain <nikhiljain90598@gmail.com>
2025-06-13 10:41:18 +0530

    Recursion Tutorial

--- Recursion/lecture2.cpp
+++ Recursion/lecture2.cpp
@@ -1,6 +1,21 @@
 #include<iostream>
 using namespace std;
+
+int tribonacci(int n){
+    if(n == 0 || n== 1){
+        return n;
+    }
+    else if(n == 2){
+        return 1;
+    }
+    else{
+        return tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3);
+    }
+}
 int main()
 {
+
+    cout<< tribonacci(7);
+
 return 0;
 }
