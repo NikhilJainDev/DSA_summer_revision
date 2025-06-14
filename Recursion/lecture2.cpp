7e274721768a98998878b20e22dfa42d632d20b4
Nikhil jain <nikhiljain90598@gmail.com>
2025-06-14 12:50:55 +0530

    Recursion

--- Recursion/lecture2.cpp
+++ Recursion/lecture2.cpp
@@ -1,21 +1,41 @@
 #include<iostream>
+#include<math.h>
 using namespace std;
 
-int tribonacci(int n){
-    if(n == 0 || n== 1){
+int sumofDigit(int n){
+    // Base Case 
+    if(n >=0 && n<=9){
         return n;
     }
-    else if(n == 2){
+    else{
+    return  sumofDigit(n / 10) + n % 10;
+    }
+    
+}
+
+int Powerof_P(int p, int q){
+    if(q == 0){
         return 1;
     }
+    if(q == 1){
+        return p;
+    }
     else{
-        return tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3);
+        return p * Powerof_P(p, q-1);
     }
 }
+
 int main()
 {
 
-    cout<< tribonacci(7);
+//    cout<< sumofDigit(8);
+//    cout<<endl;
+//   cout<<  sumofDigit(12345);
+
+cout<<Powerof_P(2, 4); cout<<endl;
+cout<<Powerof_P(3,0); cout<<endl;
+cout<<Powerof_P(4,3); cout<<endl;
+cout<<Powerof_P(17,2);
 
 return 0;
 }
