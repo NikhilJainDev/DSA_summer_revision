08bc41c8c2334032267914d48dc35086631d62cc
Nikhil jain <nikhiljain90598@gmail.com>
2025-06-26 19:41:23 +0530

    Arrays problems

---
+++ Arrays/rotate_image.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+   int n;
+   cout<<" Enter n : "<<" ";
+   cin>>n;
+   int matrix[n][n];
+    cout<<" Enter the Elements of Matrix "<<" ";
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cin>>matrix[i][j];
+        }
+    }
+
+    // printing 
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cout<<matrix[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+    cout<<endl;
+
+    int ans[n][n];
+    // Rotate by 90 degree 
+     for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            ans[j][n-i-1] = matrix[i][j];
+        }
+    }
+
+     for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cout<<ans[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+
+
+
+
+
+    return 0;
+}
