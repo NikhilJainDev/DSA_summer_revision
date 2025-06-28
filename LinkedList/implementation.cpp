a4137338af43173396cd5c600229c7bf9f3ddffd
Nikhil jain <nikhiljain90598@gmail.com>
2025-06-28 13:29:39 +0530

    LL problems

--- LinkedList/implementation.cpp
+++ LinkedList/implementation.cpp
@@ -28,11 +28,35 @@ class List{
             head = newNode;
             return;
         }
+        else{
+            newNode->next = head;
+            head = newNode;
+        }
     }
+
+    void PrintLL(){
+    Node * temp = head;
+    while(temp != NULL){
+        cout<< temp->data<<" ";
+        temp = temp->next;
+    }
+}
+
 };
+
 
 int main(){
 List ll;
+ll.push_front(11);
+ll.push_front(21);
+ll.push_front(51);
+ll.push_front(101);
+
+ll.PrintLL();
+cout<<endl;
+
+ll.push_front(523);
+ll.PrintLL();
 
     return 0;
 }
