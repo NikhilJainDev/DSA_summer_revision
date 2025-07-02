00e9d85e8e4ee7e7f5b222098a2a92899899ce06
Nikhil jain <nikhiljain90598@gmail.com>
2025-07-02 20:18:00 +0530

    Patterns Problems

--- Core Programming Questions/Patterns/type_3.cpp
+++ Core Programming Questions/Patterns/type_3.cpp
@@ -2,10 +2,28 @@
 using namespace std;
 
 void Pattern_1(){
-    
+    for(int i=1; i<=4; i++){
+        for(int j=i; j>=1; j--){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+void pattern_2(){
+    int num = 1;
+    for(int i=1; i<=4; i++){
+        for(int j=1; j<=i; j++){
+            cout<<num<<" ";
+            num = num + 1;
+        }
+        cout<<endl;
+    }
 }
 
 int main(){
-
+    Pattern_1();
+    cout<<"-----------------"<<endl;
+    pattern_2();
 return 0;
 }
