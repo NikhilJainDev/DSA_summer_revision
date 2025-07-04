24f857466479953f58120ea3e8b5b45752b6a747
Nikhil jain <nikhiljain90598@gmail.com>
2025-07-04 14:48:29 +0530

    Sets in STL

--- STL in c++/Sets.cpp
+++ STL in c++/Sets.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 #include<set>
+#include<unordered_set>
 #include<vector>
+#include<algorithm>
 using namespace std;
 
 int main()
@@ -19,6 +21,25 @@ int main()
    for(int el : st){
     cout<<el<<" ";
    }
+   cout<<endl;
+
+   // Unordered Set in STL 
+
+   unordered_set<int> s;
+   vector<int> vc = {1,5,9,8,4,2,3,4,1,11,28,96,74};
+   for(int i=0; i<vc.size(); i++){
+    s.insert(vc[i]);
+   }
+   cout<<" Elements of unordered set are -> "<<" ";
+   for(int el : s){
+    cout<<el<<" ";
+   }
+
+   cout<<endl;
+
+
+
+   
 
 
 
