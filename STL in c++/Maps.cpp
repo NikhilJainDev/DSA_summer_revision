cdb1cd0f0e466fda4bcc204af951693875f320ba
Nikhil jain <nikhiljain90598@gmail.com>
2025-07-04 15:31:13 +0530

    Maps in C++ STL

--- STL in c++/Maps.cpp
+++ STL in c++/Maps.cpp
@@ -1,8 +1,26 @@
-#include<iostream>
-#include<map>
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <map>
 using namespace std;
 int main()
 {
-   
-return 0;
+
+    // creation of map in STL c++ 
+    map<int, int> m;
+    m.insert(make_pair(10,110));
+    m.insert(make_pair(20,145));
+    m.insert(make_pair(40,160));
+    m.insert(make_pair(80,155));
+    m.insert(make_pair(40,110));   // key is duplicate so it will not insert 
+
+
+    // printing map in c++ 
+
+    for(auto it : m){
+        cout<<it.first<<" -> "<<it.second<<endl;
+    }
+
+    return 0;
+
 }
