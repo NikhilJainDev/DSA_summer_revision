b03a1596ab0530bae8bf40e67e70e86cbf981c11
Nikhil jain <nikhiljain90598@gmail.com>
2025-07-09 20:15:04 +0530

    DP Problems

--- Dynamic_Programming/climbing_stairs.cpp
+++ Dynamic_Programming/climbing_stairs.cpp
@@ -1,8 +1,10 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
 // Recursive code of Climbing stairs problem - Leetcode 70 
-int count_Ways(int i, int n){
+
+int count_Ways(int i, int n){ 
     // i = no. of ways  , n = no of floor 
     if( i == n){ // we reach to the destination 
         return 1;
@@ -13,9 +15,4 @@ int count_Ways(int i, int n){
     else{
         return count_Ways(i+1, n) + count_Ways(i+2, n);
     }
-}
-
-int main(){
-    cout<<count_Ways(0,3)<<" ways"<<endl;// steps to reach out at 5th floor 
-    return 0;
 }
